ffc60323f64715e44e38feadfee12ee26f6fda95
fidanhx <fidanqasimzade10@gmail.com>
2023-12-13 16:28:39 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const express = require("express");
+const app = express()
+
+fs.readFile('./data.json', 'utf8', (err, data) =>{
+
+  if(!err){
+    app.get("/",(req, res)=>{
+    res.send(data)
+  })
+  }
+  else{
+console.log(err)
+  }
+}
+ )
+
+app.listen(3000, ()=>{
+    console.log("done")
+  })
+
+
+
+
+     
